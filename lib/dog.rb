da7aee26d25b53200a6450f66631b47ae98813d6
DAZUN XING <dazun.xing@gmail.com>
2020-05-14 20:28:59 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,16 @@
 # Your code goes here!
+class Dog
+  
+  def name=(dogs_name)
+    @dogsname = dogs_name
+  end
+  
+  def name
+    @dogsname 
+  end
+  
+  def bark
+    puts "woof!"
+  end
+  
+end
